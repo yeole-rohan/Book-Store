b37e54f1925ca25d1f758fc0ae95826f29f05ef0
rohan yeole <rayeole@gail.com>
2023-06-22 11:20:29 +0530

    settings file change

--- famousbook/famousbook/settings.py
+++ famousbook/famousbook/settings.py
@@ -86,12 +86,24 @@ WSGI_APPLICATION = 'famousbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config['SERVERTYPE'] == "local":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'prod_book_store',
+            'USER': 'book_prod_user',
+            'PASSWORD': 'FkA46@HAU17q',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
